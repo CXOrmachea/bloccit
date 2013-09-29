d209edb51a5eafbac0067aca4eeda0d95592aa36
CXOrmachea <CXOrmachea@gmail.com>
2013-09-29 19:14:32 -0400

    Added CanCan

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,13 @@ class PostsController < ApplicationController
 
   def new
     @post=Post.new
+    authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   def create
     @post = current_user.posts.build(params[:post])
+    authorize! :create, Post, message: "You need to be a member to create a new post."
+
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -20,14 +23,17 @@ class PostsController < ApplicationController
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
     end
+
   end
 
   def edit
     @post = Post.find(params[:id])
+    authorize! :update, Post, message: "You need to be the post's dickhead to edit it"
   end
 
   def update
     @post = Post.find(params[:id])
+    authorize! :update, Post, message: "You need to be the post's dickhead to edit it"
     if @post.update_attributes(params[:post])
       flash[:notice] = "Post was updated."
       redirect_to @post
