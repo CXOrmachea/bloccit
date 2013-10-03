e3a297ec8efcbaeb179a8645f600807fdf30bc27
CXOrmachea <CXOrmachea@gmail.com>
2013-10-03 15:02:51 -0400

    Associated and nested topics and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 require 'faker'
 
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end
+
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -12,10 +20,12 @@ rand(4..10).times do
 
   rand(5..12).times do
     p = u.posts.create(
+      topic: topics[rand % topics.count], #this is different from Bloc's code
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
+
 
     rand(3..7).times do
       p.comments.create(
@@ -24,9 +34,31 @@ rand(4..10).times do
   end
 end
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: "helpfromharvard@gmail.com", password: 'helloworld', password_confirmation: 'helloworld')
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
