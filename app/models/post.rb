48d0b4989b654e9f0ba4a4d4f09b8ad664741dc0
CXOrmachea <CXOrmachea@gmail.com>
2013-10-08 17:20:30 -0400

    added post images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
   has_many :comments
-  attr_accessible :body, :title, :topic
+  attr_accessible :body, :title, :topic, :postimage
   belongs_to :user
   belongs_to :topic
+  mount_uploader :postimage, PostimageUploader
   default_scope order('created_at DESC')
 
   validates :title, length: { minimum: 5 }, presence: true
